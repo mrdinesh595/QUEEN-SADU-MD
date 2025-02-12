dabb4f81b047cd5716add832d96be778685d9d8d
mrdinesh595 <mrdineshofc@gmail.com>
2025-02-12 11:07:34 +0530

    Update imgtourl.js

--- plugins/imgtourl.js
+++ plugins/imgtourl.js
@@ -47,14 +47,14 @@ cmd({
       forwardingScore: 999,
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
-        newsletterJid: "120363354023106228@newsletter",
-        newsletterName: "JawadTechX",
+        newsletterJid: "120363354023106218@newsletter",
+        newsletterName: "ᴍʀ ᴅɪɴᴇꜱʜ",
         serverMessageId: 143
       }
     };
 
     await conn.sendMessage(from, {
-      text: `✅ *Image Uploaded Successfully 📸*\n📏 *Size:* ${imageBuffer.length} Bytes\n🔗 *URL:* ${imageUrl}\n\n> ⚖️ *Uploaded via KHAN-AI*`,
+      text: `✅ *Image Uploaded Successfully 📸*\n📏 *Size:* ${imageBuffer.length} Bytes\n🔗 *URL:* ${imageUrl}\n\n> ⚖️ *Uploaded via QUEEN-SADU*`,
       contextInfo: msgContext
     });
 
