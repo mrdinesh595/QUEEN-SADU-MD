12fd3a107c86a50719816603db7ad033c7591dcd
mrdinesh595 <mrdineshofc@gmail.com>
2025-02-12 10:26:12 +0530

    Update autovoice+autosticker+autoreply.js

--- plugins/autovoice+autosticker+autoreply.js
+++ plugins/autovoice+autosticker+autoreply.js
@@ -8,7 +8,7 @@ cmd({
   on: "body"
 },    
 async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autovoice.json');
+    const filePath = path.join(__dirname, '../data/autovoice.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
@@ -27,7 +27,7 @@ cmd({
   on: "body"
 },    
 async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autosticker.json');
+    const filePath = path.join(__dirname, '../data/autosticker.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
@@ -46,7 +46,7 @@ cmd({
   on: "body"
 },    
 async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autoreply.json');
+    const filePath = path.join(__dirname, '../data/autoreply.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
